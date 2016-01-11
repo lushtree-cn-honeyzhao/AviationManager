823384d0ced3a0386b3bf0ff791cfa91eb120bf9
zhaoguoqing <honey.zhao@aliyun.com>
2016-01-11 22:57:33 +0800

    aviationorder.jsp

--- src/com/lushapp/modules/sys/web/AviationOrderController.java
+++ src/com/lushapp/modules/sys/web/AviationOrderController.java
@@ -6,37 +6,30 @@
 package com.lushapp.modules.sys.web;
 
 import com.google.common.collect.Lists;
-import com.lushapp.common.model.Combobox;
 import com.lushapp.common.model.Datagrid;
 import com.lushapp.common.model.Result;
-import com.lushapp.common.model.TreeNode;
 import com.lushapp.common.orm.Page;
 import com.lushapp.common.orm.PropertyFilter;
 import com.lushapp.common.orm.entity.StatusState;
 import com.lushapp.common.orm.hibernate.EntityManager;
 import com.lushapp.common.utils.StringUtils;
 import com.lushapp.common.utils.collections.Collections3;
-import com.lushapp.common.utils.encode.Encrypt;
 import com.lushapp.common.utils.mapper.JsonMapper;
 import com.lushapp.common.web.springmvc.BaseController;
-import com.lushapp.modules.sys._enum.SexType;
-import com.lushapp.modules.sys.entity.*;
-import com.lushapp.modules.sys.service.*;
-import com.lushapp.utils.AppConstants;
-import com.lushapp.utils.SelectType;
+import com.lushapp.modules.sys.entity.AviationOrder;
+import com.lushapp.modules.sys.entity.User;
+import com.lushapp.modules.sys.service.AviationOrderManager;
 import org.apache.commons.lang3.ArrayUtils;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -62,7 +55,7 @@ public class AviationOrderController extends BaseController<AviationOrder,Long> 
 
     @RequestMapping(value = {""})
     public String list() {
-        return "modules/sys/aviationorder";
+        return "modules/sys/aviationOrder";
     }
 
     /**
@@ -72,7 +65,7 @@ public class AviationOrderController extends BaseController<AviationOrder,Long> 
      */
     @RequestMapping(value = {"input"})
     public String input(@ModelAttribute("model") User user) throws Exception {
-        return "modules/sys/aviationorder-input";
+        return "modules/sys/aviationOrder-input";
     }
 
 
@@ -102,6 +95,67 @@ public class AviationOrderController extends BaseController<AviationOrder,Long> 
         result = Result.successResult();
         logger.debug(result.toString());
         return result;
+    }
+
+    /**
+     * 用户combogrid所有
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = {"combogridAll"})
+    @ResponseBody
+    public String combogridAll() {
+        List<PropertyFilter> filters = Lists.newArrayList();
+        filters.add(new PropertyFilter("EQI_status", StatusState.normal.getValue().toString()));
+        List<AviationOrder> AviationOrders = aviationOrderManager.find(filters, "id", "asc");
+        Datagrid<AviationOrder> dg = new Datagrid<AviationOrder>(AviationOrders.size(), AviationOrders);
+        return JsonMapper.getInstance().toJson(dg,AviationOrder.class,new String[]{"id","loginName","name","sexView"});
+    }
+
+
+    /**
+     * combogrid
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = {"combogrid"})
+    @ResponseBody
+    public Datagrid<AviationOrder> combogrid(@RequestParam(value = "ids", required = false)List<Long> ids, String loginNameOrName, Integer rows) throws Exception {
+        Criterion statusCriterion = Restrictions.eq("status", StatusState.normal.getValue());
+        Criterion[] criterions = new Criterion[0];
+        criterions = (Criterion[]) ArrayUtils.add(criterions, 0, statusCriterion);
+        Criterion criterion = null;
+        if (Collections3.isNotEmpty(ids)) {
+            //in条件
+            Criterion inCriterion = Restrictions.in("id", ids);
+
+            if (StringUtils.isNotBlank(loginNameOrName)) {
+                Criterion loginNameCriterion = Restrictions.like("loginName", loginNameOrName, MatchMode.ANYWHERE);
+                Criterion nameCriterion = Restrictions.like("name", loginNameOrName, MatchMode.ANYWHERE);
+                Criterion criterion1 = Restrictions.or(loginNameCriterion, nameCriterion);
+                criterion = Restrictions.or(inCriterion, criterion1);
+            } else {
+                criterion = inCriterion;
+            }
+            //合并查询条件
+            criterions = (Criterion[]) ArrayUtils.add(criterions, 0, criterion);
+        } else {
+            if (StringUtils.isNotBlank(loginNameOrName)) {
+                Criterion loginNameCriterion = Restrictions.like("loginName", loginNameOrName, MatchMode.ANYWHERE);
+                Criterion nameCriterion = Restrictions.like("name", loginNameOrName, MatchMode.ANYWHERE);
+                criterion = Restrictions.or(loginNameCriterion, nameCriterion);
+                //合并查询条件
+                criterions = (Criterion[]) ArrayUtils.add(criterions, 0, criterion);
+            }
+        }
+
+        //分页查询
+        Page<AviationOrder> p = new Page<AviationOrder>(rows);//分页对象
+        p = aviationOrderManager.findByCriteria(p, criterions);
+        Datagrid<AviationOrder> dg = new Datagrid<AviationOrder>(p.getTotalCount(), p.getResult());
+        return dg;
     }
 
 
